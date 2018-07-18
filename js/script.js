24f42e1c485b6430ad77cb86319c5099f2f33edc
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-18 23:03:43 +0200

    variables name fixes

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ function Phone(brand, screen, ram, memory, price, color) {
 }
 
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", screen size is " + this.screen + ", RAM - " + this.ram + ", memory is " + this.memory + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", screen size is " + this.screen + ", RAM - " + this.ram + ", memory is " + this.memory + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
 var iPhone6S = new Phone("Apple", "4.6", "2GB", "16GB", 2250, "silver");
-var SamsungGalaxyS6 = new Phone("Samsung", "5.1", "3GB", "32GB", 1399 ,"black");
-var OnePlusOne = new Phone("OnePlus", "5.5", "3GB", "64GB", 1899 , "black");
+var samsungGalaxyS6 = new Phone("Samsung", "5.1", "3GB", "32GB", 1399 ,"black");
+var onePlusOne = new Phone("OnePlus", "5.5", "3GB", "64GB", 1899 , "black");
 
 iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
