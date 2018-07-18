cf8ce4fa8e67fbe24753e306f9ce32611519fae4
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-18 23:18:20 +0200

    added discount function

--- js/script.js
+++ js/script.js
@@ -13,10 +13,16 @@ Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", screen size is " + this.screen + ", RAM - " + this.ram + ", memory is " + this.memory + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
+Phone.prototype.applyDiscount = function(x) {
+	this.price = this.price - x;
+}
+
 var iPhone6S = new Phone("Apple", "4.6", "2GB", "16GB", 2250, "silver");
 var samsungGalaxyS6 = new Phone("Samsung", "5.1", "3GB", "32GB", 1399 ,"black");
 var onePlusOne = new Phone("OnePlus", "5.5", "3GB", "64GB", 1899 , "black");
 
 iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+onePlusOne.applyDiscount(1000);
 onePlusOne.printInfo();
